ba10b0209f2f3d5008d50dd38cd78296faf473bb
Shiraha Yuki <shirahayuki2002@gmail.com>
2026-04-02 20:10:07 +0800

    feat(std): init std lib

---
+++ packages/std/tsup.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "tsup";
+import pkg from "./package.json";
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: ["cjs", "esm"],
+  dts: true,
+  sourcemap: true,
+  clean: true,
+  minify: true,
+  target: "es2020",
+
+  external: ["@virid/core","reflect-metadata"],
+  // 添加 banner
+  banner: {
+    js: `/**
+ *  ${pkg.name} v${pkg.version}
+ *  ${pkg.description}
+ */`,
+  },
+});
