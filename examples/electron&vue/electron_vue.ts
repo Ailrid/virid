9f020cf1112e05fe74f3397a7e4adb16ea74c3f5
Ailrid <ailrid.code@gmail.com>
2026-06-26 17:49:52 +0800

    update: add new examples

---
+++ examples/electron&vue/electron_vue.ts
@@ -0,0 +1,7 @@
+/*
+ * Copyright (c) 2026-present Ailrid.
+ * Licensed under the Apache License, Version 2.0.
+ * Project: Virid
+ */
+// Electron See  https://github.com/Ailrid/vireo/tree/main/src/main
+// Vue See  https://github.com/Ailrid/vireo/tree/main/src/renderer/src
--- examples/express/express.ts
+++ examples/express/express.ts
@@ -3,4 +3,4 @@
  * Licensed under the Apache License, Version 2.0.
  * Project: Virid
  */
-//TODO
+// See  https://github.com/Ailrid/vireo/tree/main/src/main/server
